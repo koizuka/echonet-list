9266712ed4556cc5e17dc1cca10d40062a5c57cf
Akihiko Koizuka <koizuka@gmail.com>
2025-04-05 15:30:00 +0900

    refactor: websocket_client.go を4分割

---
+++ client/websocket_commands.go
@@ -0,0 +1,338 @@
+package client
+
+import (
+	"echonet-list/echonet_lite"
+	"echonet-list/protocol"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net"
+)
+
+// Discover sends a discover_devices message to the server
+func (c *WebSocketClient) Discover() error {
+	// Create the payload
+	payload := protocol.DiscoverDevicesPayload{}
+
+	// Send the message
+	_, err := c.sendRequest(protocol.MessageTypeDiscoverDevices, payload)
+	if err != nil {
+		fmt.Printf("Error discovering devices: %v\n", err)
+	}
+	return err
+}
+
+// UpdateProperties sends an update_properties message to the server
+func (c *WebSocketClient) UpdateProperties(criteria FilterCriteria) error {
+	// Get devices matching the criteria
+	devices := c.GetDevices(criteria.Device)
+	if len(devices) == 0 {
+		return fmt.Errorf("no devices match the criteria")
+	}
+
+	// Create the payload
+	targets := make([]string, 0, len(devices))
+	for _, device := range devices {
+		targets = append(targets, device.Specifier())
+	}
+
+	payload := protocol.UpdatePropertiesPayload{
+		Targets: targets,
+	}
+
+	// Send the message
+	_, err := c.sendRequest(protocol.MessageTypeUpdateProperties, payload)
+	if err != nil {
+		fmt.Printf("Error updating properties: %v\n", err)
+	}
+	return err
+}
+
+// GetProperties gets properties from a device
+func (c *WebSocketClient) GetProperties(device IPAndEOJ, EPCs []EPCType, skipValidation bool) (DeviceAndProperties, error) {
+	// Check if the device exists
+	if !skipValidation {
+		c.devicesMutex.RLock()
+		_, ok := c.devices[device.Specifier()]
+		c.devicesMutex.RUnlock()
+		if !ok {
+			return DeviceAndProperties{}, fmt.Errorf("device not found: %v", device)
+		}
+	}
+
+	// Create the payload
+	epcs := make([]string, 0, len(EPCs))
+	for _, epc := range EPCs {
+		epcs = append(epcs, fmt.Sprintf("%02X", byte(epc)))
+	}
+
+	payload := protocol.GetPropertiesPayload{
+		Targets: []string{device.Specifier()},
+		EPCs:    epcs,
+	}
+
+	// Send the message
+	response, err := c.sendRequest(protocol.MessageTypeGetProperties, payload)
+	if err != nil {
+		return DeviceAndProperties{}, err
+	}
+
+	// Parse the response
+	var resultPayload protocol.CommandResultPayload
+	if err := protocol.ParsePayload(response, &resultPayload); err != nil {
+		return DeviceAndProperties{}, fmt.Errorf("error parsing response: %v", err)
+	}
+
+	if !resultPayload.Success {
+		if resultPayload.Error != nil {
+			return DeviceAndProperties{}, fmt.Errorf("error getting properties: %s: %s", resultPayload.Error.Code, resultPayload.Error.Message)
+		}
+		return DeviceAndProperties{}, fmt.Errorf("error getting properties: unknown error")
+	}
+
+	// Parse the device data
+	var deviceData protocol.Device
+	if resultPayload.Data != nil {
+		if err := json.Unmarshal(resultPayload.Data, &deviceData); err != nil {
+			return DeviceAndProperties{}, fmt.Errorf("error parsing device data: %v", err)
+		}
+	}
+
+	// Convert protocol.Device to echonet_lite types using DeviceFromProtocol
+	ip, eoj, props, err := protocol.DeviceFromProtocol(deviceData)
+	if err != nil {
+		return DeviceAndProperties{}, fmt.Errorf("error converting device: %v", err)
+	}
+
+	// Create IPAndEOJ
+	ipAndEOJ := echonet_lite.IPAndEOJ{
+		IP:  net.ParseIP(ip),
+		EOJ: eoj,
+	}
+
+	return DeviceAndProperties{
+		Device:     ipAndEOJ,
+		Properties: props,
+	}, nil
+}
+
+// SetProperties sets properties on a device
+func (c *WebSocketClient) SetProperties(device IPAndEOJ, properties Properties) (DeviceAndProperties, error) {
+	// Create the payload
+	propsMap := make(map[string]string)
+	for _, prop := range properties {
+		propsMap[fmt.Sprintf("%02X", byte(prop.EPC))] = base64.StdEncoding.EncodeToString(prop.EDT)
+	}
+
+	payload := protocol.SetPropertiesPayload{
+		Target:     device.Specifier(),
+		Properties: propsMap,
+	}
+
+	// Send the message
+	response, err := c.sendRequest(protocol.MessageTypeSetProperties, payload)
+	if err != nil {
+		return DeviceAndProperties{}, err
+	}
+
+	// Parse the response
+	var resultPayload protocol.CommandResultPayload
+	if err := protocol.ParsePayload(response, &resultPayload); err != nil {
+		return DeviceAndProperties{}, fmt.Errorf("error parsing response: %v", err)
+	}
+
+	if !resultPayload.Success {
+		if resultPayload.Error != nil {
+			return DeviceAndProperties{}, fmt.Errorf("error setting properties: %s: %s", resultPayload.Error.Code, resultPayload.Error.Message)
+		}
+		return DeviceAndProperties{}, fmt.Errorf("error setting properties: unknown error")
+	}
+
+	// Parse the device data
+	var deviceData protocol.Device
+	if resultPayload.Data != nil {
+		if err := json.Unmarshal(resultPayload.Data, &deviceData); err != nil {
+			return DeviceAndProperties{}, fmt.Errorf("error parsing device data: %v", err)
+		}
+	}
+
+	// Convert protocol.Device to echonet_lite types using DeviceFromProtocol
+	ip, eoj, props, err := protocol.DeviceFromProtocol(deviceData)
+	if err != nil {
+		return DeviceAndProperties{}, fmt.Errorf("error converting device: %v", err)
+	}
+
+	// Create IPAndEOJ
+	ipAndEOJ := echonet_lite.IPAndEOJ{
+		IP:  net.ParseIP(ip),
+		EOJ: eoj,
+	}
+
+	return DeviceAndProperties{
+		Device:     ipAndEOJ,
+		Properties: props,
+	}, nil
+}
+
+// AliasSet sets an alias for a device
+func (c *WebSocketClient) AliasSet(alias *string, criteria FilterCriteria) error {
+	if alias == nil {
+		return fmt.Errorf("alias cannot be nil")
+	}
+
+	// Get devices matching the criteria
+	devices := c.GetDevices(criteria.Device)
+	if len(devices) == 0 {
+		return fmt.Errorf("no devices match the criteria")
+	}
+	if len(devices) > 1 {
+		return fmt.Errorf("multiple devices match the criteria")
+	}
+
+	// Create the payload
+	payload := protocol.ManageAliasPayload{
+		Action: protocol.AliasActionAdd,
+		Alias:  *alias,
+		Target: devices[0].Specifier(),
+	}
+
+	// Send the message
+	_, err := c.sendRequest(protocol.MessageTypeManageAlias, payload)
+	return err
+}
+
+// AliasDelete deletes an alias
+func (c *WebSocketClient) AliasDelete(alias *string) error {
+	if alias == nil {
+		return fmt.Errorf("alias cannot be nil")
+	}
+
+	// Create the payload
+	payload := protocol.ManageAliasPayload{
+		Action: protocol.AliasActionDelete,
+		Alias:  *alias,
+	}
+
+	// Send the message
+	_, err := c.sendRequest(protocol.MessageTypeManageAlias, payload)
+	return err
+}
+
+// GroupAdd adds devices to a group
+func (c *WebSocketClient) GroupAdd(groupName string, devices []IPAndEOJ) error {
+	// Validate the group name
+	if err := echonet_lite.ValidateGroupName(groupName); err != nil {
+		return err
+	}
+
+	// Convert devices to strings
+	deviceStrs := make([]string, 0, len(devices))
+	for _, device := range devices {
+		deviceStrs = append(deviceStrs, device.Specifier())
+	}
+
+	// Create the payload
+	payload := protocol.ManageGroupPayload{
+		Action:  protocol.GroupActionAdd,
+		Group:   groupName,
+		Devices: deviceStrs,
+	}
+
+	// Send the message
+	response, err := c.sendRequest(protocol.MessageTypeManageGroup, payload)
+	if err != nil {
+		return fmt.Errorf("error adding devices to group: %v", err)
+	}
+
+	// Parse the response
+	var resultPayload protocol.CommandResultPayload
+	if err := protocol.ParsePayload(response, &resultPayload); err != nil {
+		return fmt.Errorf("error parsing response: %v", err)
+	}
+
+	if !resultPayload.Success {
+		if resultPayload.Error != nil {
+			return fmt.Errorf("error adding devices to group: %s: %s", resultPayload.Error.Code, resultPayload.Error.Message)
+		}
+		return fmt.Errorf("error adding devices to group: unknown error")
+	}
+
+	return nil
+}
+
+// GroupRemove removes devices from a group
+func (c *WebSocketClient) GroupRemove(groupName string, devices []IPAndEOJ) error {
+	// Validate the group name
+	if err := echonet_lite.ValidateGroupName(groupName); err != nil {
+		return err
+	}
+
+	// Convert devices to strings
+	deviceStrs := make([]string, 0, len(devices))
+	for _, device := range devices {
+		deviceStrs = append(deviceStrs, device.Specifier())
+	}
+
+	// Create the payload
+	payload := protocol.ManageGroupPayload{
+		Action:  protocol.GroupActionRemove,
+		Group:   groupName,
+		Devices: deviceStrs,
+	}
+
+	// Send the message
+	response, err := c.sendRequest(protocol.MessageTypeManageGroup, payload)
+	if err != nil {
+		return fmt.Errorf("error removing devices from group: %v", err)
+	}
+
+	// Parse the response
+	var resultPayload protocol.CommandResultPayload
+	if err := protocol.ParsePayload(response, &resultPayload); err != nil {
+		return fmt.Errorf("error parsing response: %v", err)
+	}
+
+	if !resultPayload.Success {
+		if resultPayload.Error != nil {
+			return fmt.Errorf("error removing devices from group: %s: %s", resultPayload.Error.Code, resultPayload.Error.Message)
+		}
+		return fmt.Errorf("error removing devices from group: unknown error")
+	}
+
+	return nil
+}
+
+// GroupDelete deletes a group
+func (c *WebSocketClient) GroupDelete(groupName string) error {
+	// Validate the group name
+	if err := echonet_lite.ValidateGroupName(groupName); err != nil {
+		return err
+	}
+
+	// Create the payload
+	payload := protocol.ManageGroupPayload{
+		Action: protocol.GroupActionDelete,
+		Group:  groupName,
+	}
+
+	// Send the message
+	response, err := c.sendRequest(protocol.MessageTypeManageGroup, payload)
+	if err != nil {
+		return fmt.Errorf("error deleting group: %v", err)
+	}
+
+	// Parse the response
+	var resultPayload protocol.CommandResultPayload
+	if err := protocol.ParsePayload(response, &resultPayload); err != nil {
+		return fmt.Errorf("error parsing response: %v", err)
+	}
+
+	if !resultPayload.Success {
+		if resultPayload.Error != nil {
+			return fmt.Errorf("error deleting group: %s: %s", resultPayload.Error.Code, resultPayload.Error.Message)
+		}
+		return fmt.Errorf("error deleting group: unknown error")
+	}
+
+	return nil
+}
---
+++ client/websocket_notifications.go
@@ -0,0 +1,405 @@
+package client
+
+import (
+	"echonet-list/echonet_lite"
+	"echonet-list/protocol"
+	"encoding/base64"
+	"fmt"
+	"net"
+)
+
+// handleNotification handles a notification from the WebSocket server
+func (c *WebSocketClient) handleNotification(msg *protocol.Message) {
+	switch msg.Type {
+	case protocol.MessageTypeInitialState:
+		c.handleInitialState(msg)
+	case protocol.MessageTypeDeviceAdded:
+		c.handleDeviceAdded(msg)
+	case protocol.MessageTypeDeviceUpdated:
+		c.handleDeviceUpdated(msg)
+	case protocol.MessageTypeDeviceRemoved:
+		c.handleDeviceRemoved(msg)
+	case protocol.MessageTypeAliasChanged:
+		c.handleAliasChanged(msg)
+	case protocol.MessageTypeGroupChanged:
+		c.handleGroupChanged(msg)
+	case protocol.MessageTypePropertyChanged:
+		c.handlePropertyChanged(msg)
+	case protocol.MessageTypeTimeoutNotification:
+		c.handleTimeoutNotification(msg)
+	case protocol.MessageTypeErrorNotification:
+		c.handleErrorNotification(msg)
+	}
+}
+
+// handleInitialState handles an initial_state message
+func (c *WebSocketClient) handleInitialState(msg *protocol.Message) {
+	var payload protocol.InitialStatePayload
+	if err := protocol.ParsePayload(msg, &payload); err != nil {
+		if c.debug {
+			fmt.Printf("Error parsing initial_state payload: %v\n", err)
+		}
+		return
+	}
+
+	// Update devices
+	c.devicesMutex.Lock()
+	c.devices = make(map[string]echonet_lite.DeviceAndProperties)
+	for deviceID, device := range payload.Devices {
+		// Convert protocol.Device to echonet_lite types using DeviceFromProtocol
+		ip, eoj, properties, err := protocol.DeviceFromProtocol(device)
+		if err != nil {
+			if c.debug {
+				fmt.Printf("Error converting device: %v\n", err)
+			}
+			continue
+		}
+
+		// Properties are already in the correct format
+		props := properties
+
+		// Add to devices
+		ipAndEOJ := echonet_lite.IPAndEOJ{
+			IP:  net.ParseIP(ip),
+			EOJ: eoj,
+		}
+		c.devices[deviceID] = echonet_lite.DeviceAndProperties{
+			Device:     ipAndEOJ,
+			Properties: props,
+		}
+	}
+	c.devicesMutex.Unlock()
+
+	// Update aliases
+	c.aliasesMutex.Lock()
+	c.aliases = make(map[string]echonet_lite.IPAndEOJ)
+	for alias, deviceID := range payload.Aliases {
+		// Parse the device identifier
+		ipAndEOJ, err := echonet_lite.ParseDeviceIdentifier(deviceID)
+		if err != nil {
+			if c.debug {
+				fmt.Printf("Error parsing device identifier: %v\n", err)
+			}
+			continue
+		}
+
+		// Add to aliases
+		c.aliases[alias] = ipAndEOJ
+	}
+	c.aliasesMutex.Unlock()
+
+	// Update groups
+	c.groupsMutex.Lock()
+	c.groups = make([]GroupDevicePair, 0, len(payload.Groups))
+	for groupName, deviceStrs := range payload.Groups {
+		devices := make([]IPAndEOJ, 0, len(deviceStrs))
+		for _, deviceStr := range deviceStrs {
+			ipAndEOJ, err := echonet_lite.ParseDeviceIdentifier(deviceStr)
+			if err != nil {
+				if c.debug {
+					fmt.Printf("Error parsing device identifier: %v\n", err)
+				}
+				continue
+			}
+			devices = append(devices, ipAndEOJ)
+		}
+		c.groups = append(c.groups, GroupDevicePair{
+			Group:   groupName,
+			Devices: devices,
+		})
+	}
+	c.groupsMutex.Unlock()
+}
+
+// handleDeviceAdded handles a device_added message
+func (c *WebSocketClient) handleDeviceAdded(msg *protocol.Message) {
+	var payload protocol.DeviceAddedPayload
+	if err := protocol.ParsePayload(msg, &payload); err != nil {
+		if c.debug {
+			fmt.Printf("Error parsing device_added payload: %v\n", err)
+		}
+		return
+	}
+
+	// Convert protocol.Device to echonet_lite types using DeviceFromProtocol
+	ip, eoj, props, err := protocol.DeviceFromProtocol(payload.Device)
+	if err != nil {
+		if c.debug {
+			fmt.Printf("Error converting device: %v\n", err)
+		}
+		return
+	}
+
+	// Create IPAndEOJ
+	ipAndEOJ := echonet_lite.IPAndEOJ{
+		IP:  net.ParseIP(ip),
+		EOJ: eoj,
+	}
+
+	// Add to devices
+	c.devicesMutex.Lock()
+	c.devices[ipAndEOJ.String()] = echonet_lite.DeviceAndProperties{
+		Device:     ipAndEOJ,
+		Properties: props,
+	}
+	c.devicesMutex.Unlock()
+}
+
+// handleDeviceUpdated handles a device_updated message
+func (c *WebSocketClient) handleDeviceUpdated(msg *protocol.Message) {
+	var payload protocol.DeviceUpdatedPayload
+	if err := protocol.ParsePayload(msg, &payload); err != nil {
+		if c.debug {
+			fmt.Printf("Error parsing device_updated payload: %v\n", err)
+		}
+		return
+	}
+
+	// Convert protocol.Device to echonet_lite types using DeviceFromProtocol
+	ip, eoj, props, err := protocol.DeviceFromProtocol(payload.Device)
+	if err != nil {
+		if c.debug {
+			fmt.Printf("Error converting device: %v\n", err)
+		}
+		return
+	}
+
+	// Create IPAndEOJ
+	ipAndEOJ := echonet_lite.IPAndEOJ{
+		IP:  net.ParseIP(ip),
+		EOJ: eoj,
+	}
+
+	// Update devices
+	c.devicesMutex.Lock()
+	c.devices[ipAndEOJ.String()] = echonet_lite.DeviceAndProperties{
+		Device:     ipAndEOJ,
+		Properties: props,
+	}
+	c.devicesMutex.Unlock()
+}
+
+// handleDeviceRemoved handles a device_removed message
+func (c *WebSocketClient) handleDeviceRemoved(msg *protocol.Message) {
+	var payload protocol.DeviceRemovedPayload
+	if err := protocol.ParsePayload(msg, &payload); err != nil {
+		if c.debug {
+			fmt.Printf("Error parsing device_removed payload: %v\n", err)
+		}
+		return
+	}
+
+	// Parse the device identifier
+	ipAndEOJ, err := echonet_lite.ParseDeviceIdentifier(payload.IP + " " + payload.EOJ)
+	if err != nil {
+		if c.debug {
+			fmt.Printf("Error parsing device identifier: %v\n", err)
+		}
+		return
+	}
+
+	// Remove from devices
+	c.devicesMutex.Lock()
+	delete(c.devices, ipAndEOJ.String())
+	c.devicesMutex.Unlock()
+}
+
+// handleAliasChanged handles an alias_changed message
+func (c *WebSocketClient) handleAliasChanged(msg *protocol.Message) {
+	var payload protocol.AliasChangedPayload
+	if err := protocol.ParsePayload(msg, &payload); err != nil {
+		if c.debug {
+			fmt.Printf("Error parsing alias_changed payload: %v\n", err)
+		}
+		return
+	}
+
+	c.aliasesMutex.Lock()
+	defer c.aliasesMutex.Unlock()
+
+	switch payload.ChangeType {
+	case protocol.AliasChangeTypeAdded, protocol.AliasChangeTypeUpdated:
+		// Parse the device identifier
+		ipAndEOJ, err := echonet_lite.ParseDeviceIdentifier(payload.Target)
+		if err != nil {
+			if c.debug {
+				fmt.Printf("Error parsing device identifier: %v\n", err)
+			}
+			return
+		}
+
+		// Add or update the alias
+		c.aliases[payload.Alias] = ipAndEOJ
+
+	case protocol.AliasChangeTypeDeleted:
+		// Remove the alias
+		delete(c.aliases, payload.Alias)
+	}
+}
+
+// handleGroupChanged handles a group_changed message
+func (c *WebSocketClient) handleGroupChanged(msg *protocol.Message) {
+	var payload protocol.GroupChangedPayload
+	if err := protocol.ParsePayload(msg, &payload); err != nil {
+		if c.debug {
+			fmt.Printf("Error parsing group_changed payload: %v\n", err)
+		}
+		return
+	}
+
+	c.groupsMutex.Lock()
+	defer c.groupsMutex.Unlock()
+
+	switch payload.ChangeType {
+	case protocol.GroupChangeTypeAdded:
+		// グループが追加された場合
+		devices := make([]IPAndEOJ, 0, len(payload.Devices))
+		for _, deviceStr := range payload.Devices {
+			ipAndEOJ, err := echonet_lite.ParseDeviceIdentifier(deviceStr)
+			if err != nil {
+				if c.debug {
+					fmt.Printf("Error parsing device identifier: %v\n", err)
+				}
+				continue
+			}
+			devices = append(devices, ipAndEOJ)
+		}
+		c.groups = append(c.groups, GroupDevicePair{
+			Group:   payload.Group,
+			Devices: devices,
+		})
+
+	case protocol.GroupChangeTypeUpdated:
+		// グループが更新された場合
+		found := false
+		for i, group := range c.groups {
+			if group.Group == payload.Group {
+				// 既存のグループを更新
+				devices := make([]IPAndEOJ, 0, len(payload.Devices))
+				for _, deviceStr := range payload.Devices {
+					ipAndEOJ, err := echonet_lite.ParseDeviceIdentifier(deviceStr)
+					if err != nil {
+						if c.debug {
+							fmt.Printf("Error parsing device identifier: %v\n", err)
+						}
+						continue
+					}
+					devices = append(devices, ipAndEOJ)
+				}
+				c.groups[i].Devices = devices
+				found = true
+				break
+			}
+		}
+		if !found && len(payload.Devices) > 0 {
+			// グループが見つからない場合は追加
+			devices := make([]IPAndEOJ, 0, len(payload.Devices))
+			for _, deviceStr := range payload.Devices {
+				ipAndEOJ, err := echonet_lite.ParseDeviceIdentifier(deviceStr)
+				if err != nil {
+					if c.debug {
+						fmt.Printf("Error parsing device identifier: %v\n", err)
+					}
+					continue
+				}
+				devices = append(devices, ipAndEOJ)
+			}
+			c.groups = append(c.groups, GroupDevicePair{
+				Group:   payload.Group,
+				Devices: devices,
+			})
+		}
+
+	case protocol.GroupChangeTypeDeleted:
+		// グループが削除された場合
+		for i, group := range c.groups {
+			if group.Group == payload.Group {
+				// グループを削除
+				c.groups = append(c.groups[:i], c.groups[i+1:]...)
+				break
+			}
+		}
+	}
+}
+
+// handlePropertyChanged handles a property_changed message
+func (c *WebSocketClient) handlePropertyChanged(msg *protocol.Message) {
+	var payload protocol.PropertyChangedPayload
+	if err := protocol.ParsePayload(msg, &payload); err != nil {
+		if c.debug {
+			fmt.Printf("Error parsing property_changed payload: %v\n", err)
+		}
+		return
+	}
+
+	// Parse the device identifier
+	ipAndEOJ, err := echonet_lite.ParseDeviceIdentifier(payload.IP + " " + payload.EOJ)
+	if err != nil {
+		if c.debug {
+			fmt.Printf("Error parsing device identifier: %v\n", err)
+		}
+		return
+	}
+
+	// Parse the EPC
+	epc, err := echonet_lite.ParseEPCString(payload.EPC)
+	if err != nil {
+		if c.debug {
+			fmt.Printf("Error parsing EPC: %v\n", err)
+		}
+		return
+	}
+
+	// Parse the EDT
+	edt, err := base64.StdEncoding.DecodeString(payload.Value)
+	if err != nil {
+		if c.debug {
+			fmt.Printf("Error decoding EDT: %v\n", err)
+		}
+		return
+	}
+
+	// Update the property
+	c.devicesMutex.Lock()
+	if deviceProps, ok := c.devices[ipAndEOJ.String()]; ok {
+		// Create a new property
+		prop := echonet_lite.Property{
+			EPC: epc,
+			EDT: edt,
+		}
+		// Register the property
+		deviceProps.Properties = append(deviceProps.Properties, prop)
+		c.devices[ipAndEOJ.String()] = deviceProps
+	}
+	c.devicesMutex.Unlock()
+}
+
+// handleTimeoutNotification handles a timeout_notification message
+func (c *WebSocketClient) handleTimeoutNotification(msg *protocol.Message) {
+	var payload protocol.TimeoutNotificationPayload
+	if err := protocol.ParsePayload(msg, &payload); err != nil {
+		if c.debug {
+			fmt.Printf("Error parsing timeout_notification payload: %v\n", err)
+		}
+		return
+	}
+
+	if c.debug {
+		fmt.Printf("Timeout notification: %s %s: %s\n", payload.IP, payload.EOJ, payload.Message)
+	}
+}
+
+// handleErrorNotification handles an error_notification message
+func (c *WebSocketClient) handleErrorNotification(msg *protocol.Message) {
+	var payload protocol.ErrorNotificationPayload
+	if err := protocol.ParsePayload(msg, &payload); err != nil {
+		if c.debug {
+			fmt.Printf("Error parsing error_notification payload: %v\n", err)
+		}
+		return
+	}
+
+	if c.debug {
+		fmt.Printf("Error notification: %s: %s\n", payload.Code, payload.Message)
+	}
+}
