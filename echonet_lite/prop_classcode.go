2b0257019857c42f1c4b69e5149e49eda3678f8f
Akihiko Koizuka <koizuka@gmail.com>
2025-04-25 23:21:21 +0900

    chore: エコキュートのclassCode定数

--- echonet_lite/prop_classcode.go
+++ echonet_lite/prop_classcode.go
@@ -2,6 +2,7 @@ package echonet_lite
 
 const (
 	HomeAirConditioner_ClassCode     EOJClassCode = 0x0130 // 家庭用エアコン
+	ElectricWaterHeater_ClassCode    EOJClassCode = 0x026b // 電気式給湯器(エコキュート含む) (TODO)
 	FloorHeating_ClassCode           EOJClassCode = 0x027b // 床暖房
 	SingleFunctionLighting_ClassCode EOJClassCode = 0x0291 // 単機能照明
 	LightingSystem_ClassCode         EOJClassCode = 0x02a3 // 照明システム
